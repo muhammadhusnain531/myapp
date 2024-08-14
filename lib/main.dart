9192f65cdeb13fddcb9474e40cf4c94f99371408
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-08-15 02:18:42 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
-//https://github.com/iamshaunjp/flutter-beginners-tutorial
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('my first '),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]
-      ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('you clicked me');
-          },
-          icon: Icon(Icons.email),
-          color: Colors.amber,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        onPressed: () {  },
-        child: Text('click'),
-      ),
-    );
-  }
+void main(){
+  runApp(MaterialApp(home: Scaffold(
+    appBar: AppBar(
+      title: Text('First app'),
+    ),
+  ),));
 }
-
-// snippets for icons and buttons
-
-//  Icon(
-//    Icons.airport_shuttle,
-//    color: Colors.lightBlue,
-//    size: 50.0
-//  ),
-
-//  RaisedButton(
-//    onPressed: () {
-//      print('you clicked me');
-//    },
-//    child: Text('click me'),
-//    color: Colors.lightBlue,
-//  ),
-
-//  FlatButton(
-//    onPressed: () {},
-//    child: Text('click me again'),
-//    color: Colors.amber
-//  ),
-
-//  RaisedButton.icon(
-//    onPressed: () {},
-//    icon: Icon(Icons.mail),
-//    label: Text('mail me'),
-//    color: Colors.amber,
-//  ),
