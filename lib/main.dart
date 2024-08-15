812643f19bb2bdc98889f2a005035c084e68ea57
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-08-16 01:36:37 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
-
-void main(){runApp(MaterialApp(
-  home: Home(),
-));
+//16-8-2024
+void main(){
+  runApp(MaterialApp(home: Home(),));
 }
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text('Login page' , style: TextStyle(fontWeight: FontWeight.bold),),
+    return Scaffold(appBar: AppBar(title: Text('Login page' , style: TextStyle(fontWeight: FontWeight.bold),),
     ),
-    body: Padding(
-      padding: const EdgeInsets.fromLTRB(70, 200, 0, 0),
-      child: Row(
-        children: [
-          Container(child:
-          Text('Welcom to our app' ,
-            style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold , color: Colors.black),),),
-        ],
-      ),
+        body: Padding(
+        padding: const EdgeInsets.fromLTRB(70, 200, 0, 0),
+    child: Row(
+    children: [
+    Container(child:
+    Text('Welcom to our app' ,
+    style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold , color: Colors.black),),),
+    ],
+    ),
     ),
     );
   }
