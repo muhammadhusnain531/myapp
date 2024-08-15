2ca66f673eb8553128076dfefede55df3b0dd6f1
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-08-16 01:56:18 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,21 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('Login page' , style: TextStyle(fontWeight: FontWeight.bold),),
     ),
-        body: Padding(
-        padding: const EdgeInsets.fromLTRB(70, 200, 0, 0),
-    child: Row(
-    children: [
-    Container(child:
-    Text('Welcom to our app' ,
-    style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold , color: Colors.black),),),
-    ],
-    ),
-    ),
+        body:
+        Center(
+          child: Image.asset('https://miro.medium.com/v2/resize:fit:1200/1*5JFH1YSl7NHZ4kPghfXfEg.jpeg'),
+        ),
+
+    //     Padding(
+    //     padding: const EdgeInsets.fromLTRB(70, 200, 0, 0),
+    // child: Row(
+    // children: [
+    // Container(child:
+    // Text('Welcom to our app' ,
+    // style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold , color: Colors.black),),),
+    // ],
+    // ),
+    // ),
     );
   }
 }
